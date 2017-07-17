00f215a8dfac651841858a63313112b7ed513aec
Zapmaster14 <nicky5112@hotmail.com>
2017-07-17 20:25:31 +0930

    Build File Structure
    Build the base file structure and UI.

---
+++ posts.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php'; ?>
+<div class="blog-post">
+            <h2 class="blog-post-title">International PHP Conference 2014</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium commodo felis vel ultrices. Etiam id dui eros. Praesent nunc dui, volutpat at eleifend nec, suscipit a sapien. Morbi leo urna, aliquam a purus eget, tempus cursus nisl. In hac habitasse platea dictumst. Aliquam pulvinar at lorem vel vulputate. Nunc tempus enim neque. Fusce justo nibh, volutpat eget auctor et, malesuada quis odio. Suspendisse convallis consequat lectus, ullamcorper consequat mauris luctus in. Suspendisse accumsan lorem varius tincidunt tristique. Ut nulla libero, feugiat vitae dui ac, dictum adipiscing libero. Sed sit amet augue mi. Nullam luctus ligula ultricies erat volutpat scelerisque. Etiam molestie sodales aliquam. Donec nisl odio, fringilla quis nibh quis, pulvinar ornare nibh. Integer lorem ligula, scelerisque ut felis et, pretium ultricies arcu.</p>
+           <a class="readmore" href="post.php?id=1">Read More</a>
+          </div><!-- /.blog-post -->
+		  
+          <div class="blog-post">
+            <h2 class="blog-post-title">PHP 5.6.0beta4 Released</h2>
+            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium commodo felis vel ultrices. Etiam id dui eros. Praesent nunc dui, volutpat at eleifend nec, suscipit a sapien. Morbi leo urna, aliquam a purus eget, tempus cursus nisl. In hac habitasse platea dictumst. Aliquam pulvinar at lorem vel vulputate. Nunc tempus enim neque. Fusce justo nibh, volutpat eget auctor et, malesuada quis odio. Suspendisse convallis consequat lectus, ullamcorper consequat mauris luctus in. Suspendisse accumsan lorem varius tincidunt tristique. Ut nulla libero, feugiat vitae dui ac, dictum adipiscing libero. Sed sit amet augue mi. Nullam luctus ligula ultricies erat volutpat scelerisque. Etiam molestie sodales aliquam. Donec nisl odio, fringilla quis nibh quis, pulvinar ornare nibh. Integer lorem ligula, scelerisque ut felis et, pretium ultricies arcu.</p>
+          <a class="readmore" href="post.php?id=1">Read More</a>
+		  </div><!-- /.blog-post -->  
+<?php include 'includes/footer.php'; ?>
